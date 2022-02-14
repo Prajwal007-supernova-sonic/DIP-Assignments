d8ce68f0ed67f4bc395b5d79674b3fadee572f79
Prajwal007-supernova-sonic <99288170+Prajwal007-supernova-sonic@users.noreply.github.com>
2022-02-15 02:50:45 +0530

    Delete Assignment-1 directory

--- Assignment-1/binary.m
+++
@@ -1,5 +0,0 @@
-clc
-I = imread('qq1.jpg');
-imshow(I)
-Ig1 = round((I(:,:,1)+I(:,:,2)+I(:,:,3))/3);
-imshow(Ig1)
--- Assignment-1/gray_image.m
+++
@@ -1,6 +0,0 @@
-clc
-I = imread('qq1.jpg');
-imshow(I)
-size(I)
-Ig = rgb2gray(I);
-imshow(Ig)
