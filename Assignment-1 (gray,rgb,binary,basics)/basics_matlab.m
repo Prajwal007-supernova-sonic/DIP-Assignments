430cda1ddb2b7af8acd6ac83e749693bccebe7ec
Prajwal007-supernova-sonic <99288170+Prajwal007-supernova-sonic@users.noreply.github.com>
2022-02-15 02:52:36 +0530

    Add files via upload

---
+++ Assignment-1 (gray,rgb,binary,basics)/basics_matlab.m
@@ -0,0 +1,12 @@
+%% to clear
+clc
+%% to read image
+I = imread('qq1.jpg');
+%% to show output 
+imshow(I)
+%% to find dimension of images
+size(I)
+%% to find information about size, byte, class. use
+whos I
+%% to find complete information of image
+imfinfo qq1.jpg
